e1dffe4c30ade52683cfc2c31ce5c66155995110
Amr <msam.team20@gmail.com>
2024-07-22 23:11:22 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    VITE_API_TOKEN: JSON.stringify(process.env.VITE_API_TOKEN),
+    VITE_APIKEY: JSON.stringify(process.env.VITE_APIKEY),
+    VITE_AUTH_DOMAIN: JSON.stringify(process.env.VITE_AUTH_DOMAIN),
+    VITE_PROJECT_ID: JSON.stringify(process.env.VITE_PROJECT_ID),
+    VITE_STOREAGE_BUCKET: JSON.stringify(process.env.VITE_STOREAGE_BUCKET),
+    VITE_MESSAGING_SENDER_ID: JSON.stringify(process.env.VITE_MESSAGING_SENDER_ID),
+    VITE_APP_ID: JSON.stringify(process.env.VITE_APP_ID),
+    VITE_MEASUREMENT_ID: JSON.stringify(process.env.VITE_MEASUREMENT_ID)
+  }
 
 })
